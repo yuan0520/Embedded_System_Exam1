e80312791ea2e459358192cccdf103a181618b7f
yuan0520 <abcdabcd10211@gmail.com>
2021-04-07 01:24:34 -0700

    complete generate wave function

--- main.cpp
+++ main.cpp
@@ -17,8 +17,11 @@ EventQueue wave_queue(32 * EVENTS_EVENT_SIZE);
 Thread menu_t;
 Thread wave_t;
 
-int freq_pos = 3; // 0 -> 10Hz, 1 -> 25Hz, 2 => 50Hz, 3 -> 100Hz
-float freq_scale = 1; // default 100Hz
+int freq_pos = 3; // 0 -> 10ms, 1 -> 20ms, 2 => 40ms, 3 -> 80ms
+float freq_scale = 1; // default 80ms
+float wavelength_scale = 1.0;
+float S = 80;
+float len = 1.0 / (S*freq_scale);
 
 void menu(){
     uLCD.color(BLACK);
@@ -76,60 +79,28 @@ void sel_btn_task(){
 }
 
 void generate_wave(){
-
-}
-
-int main(void)
-{
-    menu_t.start(callback(&menu_queue, &EventQueue::dispatch_forever));
-    wave_t.start(callback(&wave_queue, &EventQueue::dispatch_forever));
-    btn_up.rise(&up_btn_task);
-    btn_down.rise(&down_btn_task);
-    btn_sel.rise(&sel_btn_task);
-
-    uLCD.background_color(WHITE);
-    uLCD.cls();
-    uLCD.textbackground_color(WHITE);
-    menu();
-
-    // while(1){
-    //     if(btn_up){
-    //         if(freq_pos < 3) freq_pos++;
-    //         menu(freq_pos);
-    //     }
-    //     if(btn_down){
-    //         if(freq_pos > 0) freq_pos--;
-    //         menu(freq_pos);
-    //     }
-    //     if(btn_sel){
-    //         if(freq_pos == 0) freq_scale = 0.125;
-    //         else if(freq_pos == 1) freq_scale = 0.25;
-    //         else if(freq_pos == 2) freq_scale = 0.5;
-    //         else freq_scale = 1;
-    //         break;
-    //     }
-    // }
-
-
-    float wavelength_scale = 1 / freq_scale;
-    float S = (1+0+7+0+6+0+0+0+2) % 10;
-    float up_len = 1.0 / S;
-    float down_len = 1.0 / (10-S);
-
     int idx = 0;
     int flag = 0;
 
     while (1) {
-        for (float i = 0.0f; i < wavelength_scale; i += up_len) {
-            aout = i * freq_scale;
+        for (float i = 0.0f; i < wavelength_scale; i += len) {
+            // aout = i * freq_scale;
+            aout = i;
 
             if(idx < sample){
                 ADCdata[idx++] = ain;
             }
             ThisThread::sleep_for(1ms);
         }
-        for (float j = wavelength_scale; j > 0.0f; j -= down_len){
-            aout = j * freq_scale;
+        
+        if(freq_scale == 1) ThisThread::sleep_for(80ms);
+        else if(freq_scale == 0.5) ThisThread::sleep_for(160ms);
+        else if(freq_scale == 0.25) ThisThread::sleep_for(200ms);
+        else ThisThread::sleep_for(220ms);
+
+        for (float j = wavelength_scale; j > 0.0f; j -= len){
+            // aout = j * freq_scale;
+            aout = j;
 
             if(idx < sample){
                 ADCdata[idx++] = ain;
@@ -145,4 +116,57 @@ int main(void)
             ThisThread::sleep_for(10ms); 
         }
     }
+}
+
+int main(void)
+{
+    menu_t.start(callback(&menu_queue, &EventQueue::dispatch_forever));
+    wave_t.start(callback(&wave_queue, &EventQueue::dispatch_forever));
+    btn_up.rise(&up_btn_task);
+    btn_down.rise(&down_btn_task);
+    btn_sel.rise(&sel_btn_task);
+
+    uLCD.background_color(WHITE);
+    uLCD.cls();
+    uLCD.textbackground_color(WHITE);
+    menu();
+
+    
+    // int freq_pos = 3; // 0 -> 10ms, 1 -> 20ms, 2 => 40ms, 3 -> 80ms
+    // float freq_scale = 1; // default 80ms
+    // float wavelength_scale = 1.0;
+    // float S = 80;
+    // float len = 1.0 / (S*freq_scale);
+
+    // int idx = 0;
+    // int flag = 0;
+
+    // while (1) {
+    //     for (float i = 0.0f; i < wavelength_scale; i += up_len) {
+    //         // aout = i * freq_scale;
+    //         aout = i;
+
+    //         if(idx < sample){
+    //             ADCdata[idx++] = ain;
+    //         }
+    //         ThisThread::sleep_for(1ms);
+    //     }
+    //     for (float j = wavelength_scale; j > 0.0f; j -= down_len){
+    //         // aout = j * freq_scale;
+    //         aout = j;
+
+    //         if(idx < sample){
+    //             ADCdata[idx++] = ain;
+    //         }
+    //         ThisThread::sleep_for(1ms);
+    //     }
+
+    //     if(idx >= sample && !flag){
+    //         for(int i=0; i<sample; i++){
+    //             printf("%f\r\n", ADCdata[i]);
+    //         }
+    //         flag = 1;
+    //         ThisThread::sleep_for(10ms); 
+    //     }
+    // }
 }
